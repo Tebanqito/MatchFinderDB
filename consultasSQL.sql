05313d91cc93c2855f313333c48dc8eba4b27a7e
LautaroBarrozo <lautaro-barrozo@outlook.com>
2026-07-07 18:58:41 -0300

    consultas para pruebas

---
+++ consultasSQL.sql
@@ -0,0 +1,11 @@
+SELECT * FROM User;
+SELECT * FROM Tournament;
+SELECT * FROM MatchTable;
+SELECT * FROM Friend;
+SELECT * FROM Request;
+SELECT * FROM Team5;
+SELECT * FROM Team11;
+SELECT * FROM TournamentMatch;
+
+SELECT * FROM User WHERE userName LIKE 'Carlos García';
+SELECT * FROM Friend WHERE idUser = 2
